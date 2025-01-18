3d598cd203ac3fdf92c8df7f3cbca41ccfe61529
farajzada <mfarajzadaa@gmail.com>
2025-01-18 13:09:07 +0400

    BaseEntity.java attached

--- src/model/BaseEntity.java
+++ src/model/BaseEntity.java
@@ -2,4 +2,12 @@ package model;
 
 public abstract class BaseEntity {
     private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
